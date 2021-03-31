7d256c1420af1e69d763df9041b1caa90e0ab753
Steve Gerbino <steve@gerbino.co>
2021-03-31 17:20:47 -0400

    Add koinos services to util

--- libraries/util/include/koinos/util.hpp
+++ libraries/util/include/koinos/util.hpp
@@ -8,6 +8,14 @@
 
 namespace koinos {
 
+namespace service {
+   constexpr char chain[]          = "chain";
+   constexpr char block_store[]    = "block_store";
+   constexpr char mempool[]        = "mempool";
+   constexpr char block_producer[] = "block_producer";
+   constexpr char p2p[]            = "p2p";
+}
+
 // Helper struct for using std::visit with std::variants
 template< class... Ts > struct overloaded : Ts... { using Ts::operator()...; };
 template< class... Ts > overloaded( Ts... ) -> overloaded< Ts... >;
