0278dbb73554fbf8f9cd1c9664a2c6e793f4638f
Steve Gerbino <steve@gerbino.co>
2021-08-18 12:42:41 -0400

    Use HunterGate standardized version

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,8 @@
 hunter_config(Boost
-   VERSION ${HUNTER_Boost_VERSION}
+   VERSION "1.72.0-p1"
    CMAKE_ARGS
-   USE_CONFIG_FROM_BOOST=ON
-   Boost_USE_STATIC_LIBS=ON
-   Boost_NO_BOOST_CMAKE=ON
+      USE_CONFIG_FROM_BOOST=ON
+      Boost_USE_STATIC_LIBS=ON
+      Boost_NO_BOOST_CMAKE=ON
 )
+
