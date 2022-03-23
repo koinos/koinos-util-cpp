2cf066704a384e651c7f5f83e8473e6c9aa3f16d
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-03-23 11:24:25 -0700

    Add support for repeated option fields

--- libraries/util/include/koinos/util/options.hpp
+++ libraries/util/include/koinos/util/options.hpp
@@ -7,6 +7,36 @@
 namespace koinos::util {
 
 template< typename T >
+std::vector< T > get_option(
+   std::string key,
+   std::vector< T > default_value,
+   const boost::program_options::variables_map& cli_args,
+   const YAML::Node& service_config = YAML::Node(),
+   const YAML::Node& global_config = YAML::Node() )
+{
+   if ( cli_args.count( key ) )
+      return cli_args[ key ].as< std::vector< T > >();
+
+   std::vector< T > values;
+
+   if ( service_config && service_config[ key ] )
+   {
+      const auto& config = service_config[ key ];
+      std::transform( values.end(), config.begin(), config.end(),
+         []( auto it ) -> T { return it.as< T >(); } );
+      return values;
+   }
+
+   if ( global_config && global_config[ key ] )
+   {
+      const auto& config = global_config[ key ];
+      std::transform( values.end(), config.begin(), config.end(),
+         []( auto it ) -> T { return it.as< T >(); } );
+      return values;
+   }
+}
+
+template< typename T >
 T get_option(
    std::string key,
    T default_value,
