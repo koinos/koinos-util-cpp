798d1fd9d24d320c9db838eb8b9ad853576ac5a5
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-03-23 13:34:28 -0700

    Make get_options additive

--- libraries/util/include/koinos/util/options.hpp
+++ libraries/util/include/koinos/util/options.hpp
@@ -9,22 +9,22 @@ namespace koinos::util {
 template< typename T >
 std::vector< T > get_options(
    std::string key,
-   std::vector< T > default_value,
    const boost::program_options::variables_map& cli_args,
    const YAML::Node& service_config = YAML::Node(),
    const YAML::Node& global_config = YAML::Node() )
 {
-   if ( cli_args.count( key ) )
-      return cli_args[ key ].as< std::vector< T > >();
-
    std::vector< T > values;
+
+   if ( cli_args.count( key ) )
+   {
+      values = cli_args[ key ].as< std::vector< T > >();
+   }
 
    if ( service_config && service_config[ key ] )
    {
       const auto& config = service_config[ key ];
       std::transform( config.begin(), config.end(), values.end(),
          []( auto& it ) -> T { return it.template as< T >(); } );
-      return values;
    }
 
    if ( global_config && global_config[ key ] )
@@ -32,10 +32,9 @@ std::vector< T > get_options(
       const auto& config = global_config[ key ];
       std::transform( config.begin(), config.end(), values.end(),
          []( auto& it ) -> T { return it.template as< T >(); } );
-      return values;
    }
 
-   return std::move( default_value );
+   return std::move( values );
 }
 
 template< typename T >
