6f811b7bfdd851e738d09d40f490c1284ffebd8f
Steve Gerbino <steve@gerbino.co>
2023-12-11 07:55:06 -0500

    Add format check on clang test CI build

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -4,6 +4,12 @@ set -e
 set -x
 
 if [ "$RUN_TYPE" = "test" ]; then
+
+   if [ "$CXX" = "clang++" ]; then
+      cd $TRAVIS_BUILD_DIR/build
+      cmake --build . --config Release --parallel 3 --target format.check
+   fi
+
    cd $TRAVIS_BUILD_DIR/build/tests
    exec ctest -j3 --output-on-failure
 fi
