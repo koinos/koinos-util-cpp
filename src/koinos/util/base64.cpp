f2ae826a635719a41a6fe1cb314128ddd48d9cb0
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-11 10:43:32 -0800

    Follow namespace rules in src

---
+++ src/koinos/util/base64.cpp
@@ -0,0 +1,21 @@
+#include <koinos/util/base64.hpp>
+
+#include <base64.h>
+
+#include <sstream>
+
+namespace koinos::util {
+
+template<>
+std::string to_base64< std::string >( const std::string& s, bool websafe )
+{
+  return base64_encode( s, websafe );
+}
+
+template<>
+std::string from_base64< std::string >( const std::string& s )
+{
+  return base64_decode( s );
+}
+
+} // namespace koinos::util
