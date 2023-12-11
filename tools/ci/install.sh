f2a7dabe946533dff2eced793b51eb295a8975e6
Steve Gerbino <steve@gerbino.co>
2023-12-11 05:48:49 -0500

    Avoid using special Ubuntu packages

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -yq --allow-downgrades libc6=2.31-0ubuntu9.2 libc6-dev=2.31-0ubuntu9.2
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends --allow-downgrades --allow-remove-essential --allow-change-held-packages install clang-11 llvm-11 -o Debug::pkgProblemResolver=yes
+sudo apt install -y clang llvm
 
 if [ "$RUN_TYPE" = "coverage" ]; then
    sudo apt-get install -y lcov ruby
