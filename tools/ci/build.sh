5b41813d99ba7af7208b7f0789f1a272f0c39fb3
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-14 14:56:15 -0800

    Add sanitze travis build

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 mkdir build
-cd build
+pushd build
 
 if [ "$RUN_TYPE" = "test" ]; then
    cmake -DCMAKE_BUILD_TYPE=Release ..
@@ -16,4 +16,22 @@ elif [ "$RUN_TYPE" = "coverage" ]; then
 elif [ "$RUN_TYPE" = "static-analysis" ]; then
    cmake -DCMAKE_BUILD_TYPE=Debug -DSTATIC_ANALYSIS=ON ..
    cmake --build . --config Debug --parallel 3
+elif [ "$RUN_TYPE" = "sanitizer" ]; then
+   popd
+   mkdir build-address
+   pushd build-address
+   cmake -DCMAKE_BUILD_TYPE=Release -DSANITIZER=Address ..
+   cmake --build . --config Release --parallel 3
+
+   popd
+   mkdir build-stack
+   pushd build-stack
+   cmake -DCMAKE_BUILD_TYPE=Release -DSANITIZER=Stack ..
+   cmake --build . --config Release --parallel 3
+
+   popd
+   mkdir build-thread
+   pushd build-thread
+   cmake -DCMAKE_BUILD_TYPE=Release -DSANITIZER=Thread ..
+   cmake --build . --config Release --parallel 3
 fi
--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -9,5 +9,16 @@ if [ "$RUN_TYPE" = "test" ]; then
 
    cd $TRAVIS_BUILD_DIR/build/tests
    exec ctest -j3 --output-on-failure
+else if [ "$RUN_TYPE" = "sanitizer" ]; then
+   pushd $TRAVIS_BUILD_DIR/build-address/tests
+   exec ctest -j1 --output-on-failure
+
+   popd
+   pushd $TRAVIS_BUILD_DIR/build-stack/tests
+   exec ctest -j1 --output-on-failure
+
+   popd
+   pushd $TRAVIS_BUILD_DIR/build-thread/tests
+   exec ctest -j1 --output-on-failure
 fi
 
