7146a977502f366aeb9f8ea64236f4ded4ddbe94
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-15 08:56:53 -0800

    Cleanup

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -17,11 +17,6 @@ elif [ "$RUN_TYPE" = "static-analysis" ]; then
    cmake -DCMAKE_BUILD_TYPE=Debug -DSTATIC_ANALYSIS=ON ..
    cmake --build . --config Debug --parallel 3
 elif [ "$RUN_TYPE" = "sanitizer" ]; then
-   ls -la /usr/lib
-   sudo ln /usr/lib/LLVMgold.so /usr/local/clang-16.0.0/lib/LLVMgold.so
-   ls -la /usr/local/clang-16.0.0/lib
-   ls -la /usr/local/clang-16.0.0/bin/../lib/
-
    popd
    mkdir build-address
    pushd build-address
